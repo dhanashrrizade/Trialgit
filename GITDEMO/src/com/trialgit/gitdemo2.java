e25fd4f15d2f567edd9d7eb4ddbc37892b9d030a
dhajrt <aishwaryazade24@gmail.com>
2024-02-05 22:41:09 +0530

    this is new repo

--- GITDEMO/src/com/trialgit/gitdemo2.java
+++ GITDEMO/src/com/trialgit/gitdemo2.java
@@ -1,5 +1,26 @@
 package com.trialgit;
+import java.util.ArrayList;
 
 public class gitdemo2 {
+	
+
+	
+	    public static void main(String[] args) {
+	        // Declare and initialize an ArrayList of Integers
+	        ArrayList<Integer> integerList = new ArrayList<>();
+
+	        // Adding elements to the ArrayList
+	        integerList.add(5);
+	        integerList.add(10);
+	        integerList.add(15);
+	        integerList.add(20);
+
+	        // Display the elements of the ArrayList
+	        System.out.println("Elements in the ArrayList:");
+	        for (int number : integerList) {
+	            System.out.println(number);
+	        }
+	    }
+	}
 
 }
